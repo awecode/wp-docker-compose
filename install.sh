0590168f7e50ea9aed923fc31689e921f87d9d72
Dipesh Acharya <dipesh@awecode.com>
2025-02-17 15:44:56 +0545

    fix: fix url rewrites

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 docker exec -i $(docker compose ps -q db) mysql -u root -prootpasswordchangeme -e "DROP DATABASE IF EXISTS wordpress; CREATE DATABASE wordpress;"
 docker exec -i $(docker compose ps -q db) mysql -u wordpress -pwordpresspasswordchangeme wordpress < dump.sql
+docker exec -i $(docker compose ps -q db) mysql -u wordpress -pwordpresspasswordchangeme wordpress -e "UPDATE wp_options SET option_value='http://localhost:8080' WHERE option_name='siteurl' OR option_name='home';"
